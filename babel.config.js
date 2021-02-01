9a7c660fd785a5884312c14ca8d89e8a81eed463
zahra <zahra@yunio.com>
2021-02-01 14:19:42 +0800

    生产环境打包去掉console

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,16 @@
+const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV)
+const plugins = []
+if (IS_PROD) {
+  plugins.push('transform-remove-console')
+}
+plugins.push['import', {
+  'libraryName': 'ant-design-vue',
+  'libraryDirectory': 'es',
+  'style': 'css' // `style: true` 会加载 less 文件
+}]
 module.exports = {
   "presets": [
     "@vue/cli-plugin-babel/preset"
   ],
-  "plugins": [
-    [
-      "import",
-      {
-        "libraryName": "ant-design-vue",
-        "libraryDirectory": "es",
-        "style": "css"
-      }
-    ]
-  ]
+  plugins
 }
